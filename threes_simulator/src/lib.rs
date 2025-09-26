8dc99a87ca02f5ad297521392f5c3fad10b9bc84
Nathan Arthur <narthur-git@rainskit.com>
2025-09-26 14:35:25 -0400

    Initial unit tests

--- threes_simulator/src/lib.rs
+++ threes_simulator/src/lib.rs
@@ -4,30 +4,6 @@ use rand::thread_rng;
 
 type Card = u16;
 
-// #[derive(Clone, Copy, Hash, PartialEq, Eq)]
-#[derive(Debug)]
-struct BoardState {
-    board: [[Card; 4]; 4],
-}
-
-impl BoardState {
-    fn initialize(draw_pile: &mut DrawPile, rng: &mut ThreadRng) -> BoardState {
-        let mut board: Vec<Card> = draw_pile.take(9).collect();
-        let mut empties = vec![0; 7];
-        board.append(&mut empties);
-        board.shuffle(rng);
-
-        let board: [[Card; 4]; 4] = [
-            board[0..4].try_into().unwrap(),
-            board[4..8].try_into().unwrap(),
-            board[8..12].try_into().unwrap(),
-            board[12..16].try_into().unwrap(),
-        ];
-
-        BoardState { board }
-    }
-}
-
 #[derive(Debug)]
 struct DrawPile {
     cards: Vec<Card>,
@@ -51,6 +27,30 @@ impl Iterator for DrawPile {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.cards.pop()
+    }
+}
+
+// #[derive(Clone, Copy, Hash, PartialEq, Eq)]
+#[derive(Debug)]
+struct BoardState {
+    board: [[Card; 4]; 4],
+}
+
+impl BoardState {
+    fn initialize(draw_pile: &mut DrawPile, rng: &mut ThreadRng) -> BoardState {
+        let mut board: Vec<Card> = draw_pile.take(9).collect();
+        let mut empties = vec![0; 7];
+        board.append(&mut empties);
+        board.shuffle(rng);
+
+        let board: [[Card; 4]; 4] = [
+            board[0..4].try_into().unwrap(),
+            board[4..8].try_into().unwrap(),
+            board[8..12].try_into().unwrap(),
+            board[12..16].try_into().unwrap(),
+        ];
+
+        BoardState { board }
     }
 }
 
@@ -90,8 +90,69 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn draw_pile_initialize() {
+        let mut rng = thread_rng();
+
+        let bonus_pile = DrawPile::initialize(&mut rng, false);
+        assert_eq!(bonus_pile.cards.len(), 0, "bonus pile is empty");
+
+        let main1 = DrawPile::initialize(&mut rng, true);
+        assert_eq!(main1.cards.len(), 12, "main pile has 12 cards");
+
+        for value in 1..=3 {
+            let value_count = main1.cards.iter().filter(|each| **each == value).count();
+            assert_eq!(4, value_count, "4 cards with value {value}");
+        }
+
+        let main2 = DrawPile::initialize(&mut rng, true);
+        assert_ne!(main1.cards, main2.cards, "main piles are shuffled");
+    }
+
+    /*
+     * GameState:
+     *      initialize:
+     *          works
+     */
+
+    #[test]
+    fn board_state_initialize() {
+        let mut rng = thread_rng();
+
+        let mut main1 = DrawPile::initialize(&mut rng, true);
+        let board1 = BoardState::initialize(&mut main1, &mut rng);
+
+        assert_eq!(3, main1.cards.len(), "draw pile had 9 cards removed");
+
+        let mut main2 = DrawPile::initialize(&mut rng, true);
+        let board2 = BoardState::initialize(&mut main2, &mut rng);
+
+        assert_ne!(board1.board, board2.board, "boards are shuffled");
+
+        let (mut zeros, mut ones, mut twos, mut threes) = (0, 0, 0, 0);
+        for &card in board1.board.iter().flatten() {
+            match card {
+                0 => zeros += 1,
+                1 => ones += 1,
+                2 => twos += 1,
+                3 => threes += 1,
+                _ => {
+                    assert!(false, "this shouldn't be possible")
+                }
+            }
+        }
+
+        assert_eq!(7, zeros, "7 empty cards");
+        assert!(ones > 0, "at least one 1");
+        assert!(twos > 0, "at least one 2");
+        assert!(threes > 0, "at least one 3");
+        assert_eq!(9, ones + twos + threes, "9 non-empty cards");
+    }
+
+    #[test]
+    #[should_panic]
+    fn board_state_initialize_panic_on_short_pile() {
+        let mut rng = thread_rng();
+        let mut empty_pile = DrawPile::initialize(&mut rng, false);
+        let _ = BoardState::initialize(&mut empty_pile, &mut rng);
     }
 }
