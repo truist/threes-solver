a194fd2536bda00ccdc6ba52768febdb85a5fe7d
Nathan Arthur <narthur-git@rainskit.com>
2025-11-25 21:26:12 -0500

    Add tests for neighbors.rs

--- threes_solver/src/algo/neighbors.rs
+++ threes_solver/src/algo/neighbors.rs
@@ -62,3 +62,71 @@ where
         f(i, grid[i], [n0, n1, n2, n3]);
     }
 }
+
+/************ tests *************/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_iterate_with_neighbors() {
+        let grid = [
+             1,  2,  3,  4,
+             5,  6,  7,  8,
+             9, 10, 11, 12,
+            13, 14, 15, 16
+        ];
+
+        let mut seen = [false; 16];
+
+        iterate_with_neighbors(&grid, |index, card, neighbors| {
+            match index {
+                0 => check(index, card, [Card::MAX, Card::MAX, 2, 5], neighbors),
+                1 => check(index, card, [1, Card::MAX, 3, 6], neighbors),
+                2 => check(index, card, [2, Card::MAX, 4, 7], neighbors),
+                3 => check(index, card, [3, Card::MAX, Card::MAX, 8], neighbors),
+                4 => check(index, card, [Card::MAX, 1, 6, 9], neighbors),
+                5 => check(index, card, [5, 2, 7, 10], neighbors),
+                6 => check(index, card, [6, 3, 8, 11], neighbors),
+                7 => check(index, card, [7, 4, Card::MAX, 12], neighbors),
+                8 => check(index, card, [Card::MAX, 5, 10, 13], neighbors),
+                9 => check(index, card, [9, 6, 11, 14], neighbors),
+                10 => check(index, card, [10, 7, 12, 15], neighbors),
+                11 => check(index, card, [11, 8, Card::MAX, 16], neighbors),
+                12 => check(index, card, [Card::MAX, 9, 14, Card::MAX], neighbors),
+                13 => check(index, card, [13, 10, 15, Card::MAX], neighbors),
+                14 => check(index, card, [14, 11, 16, Card::MAX], neighbors),
+                15 => check(index, card, [15, 12, Card::MAX, Card::MAX], neighbors),
+                unexpected => assert!(false, "Unexpected board index: {unexpected}"),
+            }
+            seen[index] = true;
+        });
+
+        for position in 0..15 {
+            assert_eq!(
+                true, seen[position],
+                "We were called for board position {}",
+                position
+            );
+        }
+    }
+
+    fn check(
+        index: usize,
+        actual_value: Card,
+        expected_neighbors: [Card; NUM_NEIGHBORS],
+        actual_neighbors: [Card; NUM_NEIGHBORS],
+    ) {
+        assert_eq!(
+            index as u16 + 1,
+            actual_value,
+            "Card is as expected at board position {index}"
+        );
+        assert_eq!(
+            expected_neighbors, actual_neighbors,
+            "Neighbors are as expected at board position {index}"
+        );
+    }
+}
--- threes_solver/src/algo/weighted.rs
+++ threes_solver/src/algo/weighted.rs
@@ -11,12 +11,15 @@ pub struct WeightedAlgo<A: ?Sized> {
 impl<A: Algo + ?Sized> WeightedAlgo<A> {
     pub fn score(&self, game_state: &Option<GameState>) -> f64 {
         let score = self.algo.score(game_state);
+
+        // this is just a "canary" in case we implement an algo that risks overflowing an i8
         assert!(
             // this 10 is a hack to get around scaled HighWall pushing the limits
             score <= i8::MAX / 2 + 10,
             "{:?} is getting dangerously close to the i8 score limit (got score of {score})\n{game_state:#?}",
             self.algo
         );
+
         score as f64 * self.weight
     }
 }
