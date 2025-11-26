68daa99834cf522714ec47c9d3158ffca3e56b20
Nathan Arthur <narthur-git@rainskit.com>
2025-11-26 06:27:12 -0500

    Fix bug and delete dead code in rng_util

--- rng_util/src/lib.rs
+++ rng_util/src/lib.rs
@@ -1,8 +1,6 @@
 pub use rand::seq::{IndexedRandom, IteratorRandom, SliceRandom};
 pub use rand::{Rng, RngCore, SeedableRng};
 
-use std::num::ParseIntError;
-
 pub type RngType = rand_xoshiro::Xoshiro256PlusPlus;
 
 pub fn rng_from_seed(seed: u64) -> RngType {
@@ -13,21 +11,13 @@ pub fn seed_from_entropy() -> u64 {
     rand::rng().random()
 }
 
-pub fn parse_seed(s: &str) -> Result<u64, ParseIntError> {
-    if let Some(hex) = s.strip_prefix("0x") {
-        u64::from_str_radix(hex, 16)
-    } else {
-        u64::from_str_radix(s, 16).or_else(|_| s.parse::<u64>())
-    }
-}
-
 pub fn initialize_rng(seed: Option<u64>) -> (RngType, u64) {
     let seed = if let Some(val) = seed {
-        println!("Using user-provided seed: 0x{val:016x}");
+        println!("Using user-provided seed: {val}");
         val
     } else {
         let seed = seed_from_entropy();
-        println!("Generated random seed: 0x{seed:016x}");
+        println!("Generated random seed: {seed}");
         seed
     };
 
