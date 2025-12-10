cf5a94cbe8dfd718d877a9398989ce9f252ceecb
Nathan Arthur <narthur-git@rainskit.com>
2025-12-10 07:23:27 -0500

    Set up to debug the crash

--- threes_solver/optimize.sh
+++ threes_solver/optimize.sh
@@ -2,7 +2,20 @@
 
 set -eu
 
-caffeinate -d -- cargo run --release -- optimize "$@"
+# caffeinate -d -- cargo run --release -- optimize "$@"
+
+# DYLD_INSERT_LIBRARIES=/Users/truist/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/x86_64-apple-darwin/lib/librustc-nightly_rt.asan.dylib \
+# RUSTFLAGS="-Z sanitizer=address -C debuginfo=2" \
+# cargo +nightly run --release -- optimize "$@"
+
+RUSTFLAGS="-C debuginfo=2" cargo build --release
+
+MallocNanoZone=0 \
+MallocScribble=1 \
+MallocGuardEdges=1 \
+lldb ../target/release/threes_solver -- --seed 3696195633902677747 optimize
+# MallocCheckHeapStart=1 \
+# MallocCheckHeapEach=1 \
 
 if [[ $? -eq 0 ]]; then
 	say "Optimization succeeded"
