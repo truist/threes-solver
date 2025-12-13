truist/threes-solver

commit e2e5221b9b1ff76f1abfbfabfb6c34d44e40f4f5

Difficult but satisfying refactor to make each Algo its own struct

threes_solver/src/algo/impls/squeezes.rs
@@ -1,17 +1,16 @@
 use threes_simulator::game_state::{Card, Direction, GameState};
 
-use crate::algo::core::Algos;
+use crate::algo::core::{Algo, Algos};
 
 use super::super::core::ValueBooster;
 use super::super::neighbors::iterate_with_neighbors;
 
-impl Algos {
-    // a smaller card "squeezed" between bigger cards and/or the wall
-    pub(crate) fn squeezes(
-        &self,
-        game_state: &GameState,
-        booster: Option<&dyn ValueBooster>,
-    ) -> f64 {
+// a smaller card "squeezed" between bigger cards and/or the wall
+#[derive(Debug)]
+pub(crate) struct Squeezes;
+
+impl Algo for Squeezes {
+    fn score(&self, game_state: &GameState, booster: Option<&dyn ValueBooster>) -> f64 {
         let mut score = 0.0;
 
         iterate_with_neighbors(game_state.get_grid(), |_index, card, neighbors| {
@@ -38,6 +37,12 @@ impl Algos {
         score
     }
 
+    fn normalization_factor(&self) -> f64 {
+        Algos::ALGO_MAX_BASE / 16.0
+    }
+}
+
+impl Squeezes {
     fn is_pair_squeezing(&self, middle: Card, pair: (Card, Card)) -> bool {
         let (m, (a, b)) = (middle, pair);
 
@@ -50,7 +55,7 @@ impl Algos {
 
 #[cfg(test)]
 mod tests {
-    use crate::algo::core::Algos::Squeezes;
+    use super::*;
 
     use super::super::super::test_utils::generate_game_state;
     use super::super::super::wrappers::value_booster_wrapper::ValueBoosterWrapper;
@@ -61,7 +66,7 @@ mod tests {
         let game_state = generate_game_state([3; 16]);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "no squeezes when everything is mergeable"
         );
 
@@ -70,7 +75,7 @@ mod tests {
         let game_state = generate_game_state(grid);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "no squeezes when just one cell filled"
         );
 
@@ -82,7 +87,7 @@ mod tests {
         ]);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "no squeezes when cards don't have neighbors"
         );
 
@@ -94,7 +99,7 @@ mod tests {
         ]);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "no squeezes when squeezed cards are mergeable"
         );
 
@@ -106,7 +111,7 @@ mod tests {
         ]);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "1s and 2s can't squeeze each other"
         );
 
@@ -118,7 +123,7 @@ mod tests {
         ]);
         assert_eq!(
             2.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "1s and 2s can be squeezed by >= 3"
         );
 
@@ -130,7 +135,7 @@ mod tests {
         ]);
         assert_eq!(
             0.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "no squeezes when bigger neighbors aren't on opposite sides"
         );
 
@@ -142,7 +147,7 @@ mod tests {
         ]);
         assert_eq!(
             4.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "obvious squeezes in both directions"
         );
 
@@ -154,7 +159,7 @@ mod tests {
         ]);
         assert_eq!(
             4.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "you can be squeezed between a card and the wall, in all four directions"
         );
 
@@ -166,7 +171,7 @@ mod tests {
         ]);
         assert_eq!(
             1.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "the three is squeezed but I'm not sure it should be"
             // TODO handle this case better?
         );
@@ -179,7 +184,7 @@ mod tests {
         ]);
         assert_eq!(
             1.0,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "the three counts as squeezed but that seems wrong"
             // TODO handle this case better?
         );
@@ -193,13 +198,13 @@ mod tests {
         ]);
         assert_eq!(
             (2 + 2 + 1 + 0 + 0 + 0 + 1 + 2) as f64,
-            Squeezes.squeezes(&game_state, None),
+            Squeezes.score(&game_state, None),
             "a big complex example"
         );
 
         let test_boost = 2.5;
         let booster = ValueBoosterWrapper {
-            wrapped: Squeezes,
+            wrapped: Box::new(Squeezes),
             min_value_to_boost: 1,
             max_value_to_boost: 6,
             boost: test_boost,
@@ -217,7 +222,7 @@ mod tests {
         assert_eq!(
             1.0 * test_boost + 1.0 * test_boost + 2.0 * test_boost + 1.0
                 + 1.0 * test_boost + 0.0 + 1.0 + 1.0 * test_boost + 0.0,
-            Squeezes.squeezes(&game_state, Some(&booster)),
+            Squeezes.score(&game_state, Some(&booster)),
             "a big complex boosted example, covering a bunch of cases"
         );
     }
@@ -231,7 +236,7 @@ mod tests {
             6, 6, 6, 6,
             3, 3, 3, 3,
         ]);
-        assert_eq!(8.0, Squeezes.squeezes(&game_state, None), "squeezes max score, first case");
+        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, first case");
 
         // 16
         let game_state = generate_game_state([
@@ -240,7 +245,7 @@ mod tests {
             3, 6, 3, 6,
             6, 3, 6, 3,
         ]);
-        assert_eq!(8.0 * 2.0, Squeezes.squeezes(&game_state, None), "squeezes max score, second case");
+        assert_eq!(8.0 * 2.0, Squeezes.score(&game_state, None), "squeezes max score, second case");
 
         let game_state = generate_game_state([
             3, 6, 3, 6,
@@ -248,7 +253,7 @@ mod tests {
             3, 6, 3, 6,
             3, 6, 3, 6,
         ]);
-        assert_eq!(8.0, Squeezes.squeezes(&game_state, None), "squeezes max score, third case");
+        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, third case");
 
         let game_state = generate_game_state([
             3, 3, 3, 3,
@@ -256,6 +261,6 @@ mod tests {
             3, 6, 6, 3,
             3, 3, 3, 3,
         ]);
-        assert_eq!(8.0, Squeezes.squeezes(&game_state, None), "squeezes max score, fourth case");
+        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, fourth case");
     }
 }

threes_solver/src/algo/core.rs
@@ -5,7 +5,8 @@ use strum_macros::EnumIter;
 
 use threes_simulator::game_state::{Card, GameState};
 
-use super::wrappers::{
+use crate::algo::impls::*;
+use crate::algo::wrappers::{
     few_empties_scaler::FewEmptiesScaler, moves_scaler::MovesScaler,
     value_booster_wrapper::ValueBoosterWrapper,
 };
@@ -25,25 +26,32 @@ pub struct AlgoConfig {
     pub boost_few_empties_12: bool,
 }
 
-pub trait ValueBooster: std::fmt::Debug + std::fmt::Display {
+pub trait ValueBooster: fmt::Debug + fmt::Display {
     fn boost_score_for(&self, score: f64, values: &[Card]) -> f64;
 }
 
-pub(crate) fn assert_value_booster_not_supported(
-    caller: &Algos,
-    value_booster: Option<&dyn ValueBooster>,
-) {
-    assert!(
-        value_booster.is_none(),
-        "{caller:?} does not support ValueBooster"
-    );
-}
-
-pub trait Algo: std::fmt::Debug + std::fmt::Display + Send + Sync {
+pub trait Algo: fmt::Debug + Send + Sync {
     fn score(&self, game_state: &GameState, value_booster: Option<&dyn ValueBooster>) -> f64;
 
     // TODO: unit tests for all the implementations
     fn normalization_factor(&self) -> f64;
+
+    fn assert_value_booster_not_supported(&self, value_booster: Option<&dyn ValueBooster>) {
+        assert!(
+            value_booster.is_none(),
+            "{self:?} does not support ValueBooster"
+        );
+    }
+
+    fn fmt_impl(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl fmt::Display for dyn Algo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.fmt_impl(f)
+    }
 }
 
 #[derive(Clone, Copy, Debug, EnumIter)]
@@ -52,42 +60,29 @@ pub enum Algos {
     Merges,
     NearlyMerges,
     Squeezes,
-    HighWall,
-    HighCorner,
+    HighWalls,
+    HighCorners,
     Monotones,
-}
-
-impl Algo for Algos {
-    fn score(&self, game_state: &GameState, booster: Option<&dyn ValueBooster>) -> f64 {
-        match self {
-            Algos::Empties => self.empties(game_state, booster),
-            Algos::Merges => self.merges(game_state, booster),
-            Algos::NearlyMerges => self.nearly_merges(game_state, booster),
-            Algos::Squeezes => self.squeezes(game_state, booster) * -1.0,
-            Algos::HighWall => self.high_walls(game_state, booster),
-            Algos::HighCorner => self.high_corners(game_state, booster),
-            Algos::Monotones => self.monotones(game_state, booster),
-        }
-    }
-
-    fn normalization_factor(&self) -> f64 {
-        match self {
-            Algos::Empties => Algos::ALGO_MAX_BASE / 16.0,
-            Algos::Merges => Algos::ALGO_MAX_BASE / 24.0,
-            Algos::NearlyMerges => Algos::ALGO_MAX_BASE / 24.0,
-            Algos::Squeezes => Algos::ALGO_MAX_BASE / 16.0,
-            Algos::HighWall => Algos::ALGO_MAX_BASE / 24.0,
-            Algos::HighCorner => Algos::ALGO_MAX_BASE / 12.0,
-            Algos::Monotones => Algos::ALGO_MAX_BASE / 24.0,
-        }
-    }
 }
 
 impl Algos {
     pub const ALGO_MAX_BASE: f64 = 24.0;
 
-    // This gives us compiler guarantees that we haven't missed any cases,
-    // and an easy way to toggle cases on and off.
+    pub fn to_algo(&self) -> Box<dyn Algo> {
+        match self {
+            Algos::Empties => Box::new(empties::Empties),
+            Algos::Merges => Box::new(merges::Merges),
+            Algos::NearlyMerges => Box::new(merges::NearlyMerges),
+            Algos::Squeezes => Box::new(squeezes::Squeezes),
+            Algos::HighWalls => Box::new(highs::HighWalls),
+            Algos::HighCorners => Box::new(highs::HighCorners),
+            Algos::Monotones => Box::new(monotones::Monotones),
+        }
+    }
+
+    // This seems like it should be a fn on Algo but I'm leaving it here because
+    // it gives us compiler guarantees that we haven't missed any cases,
+    // and an easy way to toggle cases on and off all in one file.
     pub fn default_config(&self) -> AlgoConfig {
         match self {
             Algos::Empties => AlgoConfig {
@@ -146,7 +141,7 @@ impl Algos {
                 boost_few_empties: false,
                 boost_few_empties_12: false,
             },
-            Algos::HighWall => AlgoConfig {
+            Algos::HighWalls => AlgoConfig {
                 base: false,
                 time_positive: false,
                 time_negative: false,
@@ -160,7 +155,7 @@ impl Algos {
                 boost_few_empties: false,
                 boost_few_empties_12: false, // what does this even mean?
             },
-            Algos::HighCorner => AlgoConfig {
+            Algos::HighCorners => AlgoConfig {
                 base: false,
                 time_positive: false,
                 time_negative: false,
@@ -192,82 +187,73 @@ impl Algos {
     }
 }
 
-impl fmt::Display for Algos {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
 pub fn build_all_algos() -> Vec<Box<dyn Algo>> {
     let mut all_algos: Vec<Box<dyn Algo>> = Vec::new();
 
-    for algo in Algos::iter() {
-        let config = algo.default_config();
+    for algos in Algos::iter() {
+        let config = algos.default_config();
 
         if config.base {
-            all_algos.push(algo_box(algo));
+            all_algos.push(algos.to_algo());
         }
         if config.time_positive {
-            all_algos.push(algo_box(scale(algo, true)));
+            all_algos.push(scale(algos.to_algo(), true));
         }
         if config.time_negative {
-            all_algos.push(algo_box(scale(algo, false)));
+            all_algos.push(scale(algos.to_algo(), false));
         }
 
         if config.boost_12 {
-            // all_algos.push(algo_box(booster(algo, 1, 2, 2.0)));
-            all_algos.push(algo_box(booster(algo, 1, 2, 3.0)));
-            // all_algos.push(algo_box(booster(algo, 1, 2, 4.0)));
+            // all_algos.push(booster(algos.to_algo(), 1, 2, 2.0));
+            all_algos.push(booster(algos.to_algo(), 1, 2, 3.0));
+            // all_algos.push(booster(algos.to_algo(), 1, 2, 4.0));
         }
         if config.time_positive_boost_12 {
-            all_algos.push(algo_box(scale(booster(algo, 1, 2, 2.0), true)));
-            // all_algos.push(algo_box(scale(booster(algo, 1, 2, 3.0), true)));
-            // all_algos.push(algo_box(scale(booster(algo, 1, 2, 4.0), true)));
+            all_algos.push(scale(booster(algos.to_algo(), 1, 2, 2.0), true));
+            // all_algos.push(scale(booster(algos.to_algo(), 1, 2, 3.0), true));
+            // all_algos.push(scale(booster(algos.to_algo(), 1, 2, 4.0), true));
         }
         if config.time_negative_boost_12 {
-            // all_algos.push(algo_box(scale(booster(algo, 1, 2, 2.0), false)));
-            all_algos.push(algo_box(scale(booster(algo, 1, 2, 3.0), false)));
-            // all_algos.push(algo_box(scale(booster(algo, 1, 2, 4.0), false)));
+            // all_algos.push(scale(booster(algos.to_algo(), 1, 2, 2.0), false));
+            all_algos.push(scale(booster(algos.to_algo(), 1, 2, 3.0), false));
+            // all_algos.push(scale(booster(algos.to_algo(), 1, 2, 4.0), false));
         }
 
         if config.boost_high {
-            // all_algos.push(algo_box(booster(algo, 96, 6144, 2.0)));
-            all_algos.push(algo_box(booster(algo, 96, 6144, 3.0)));
-            // all_algos.push(algo_box(booster(algo, 96, 6144, 4.0)));
+            // all_algos.push(booster(algos.to_algo(), 96, 6144, 2.0));
+            all_algos.push(booster(algos.to_algo(), 96, 6144, 3.0));
+            // all_algos.push(booster(algos.to_algo(), 96, 6144, 4.0));
         }
 
         if config.boost_few_empties {
-            all_algos.push(algo_box(empties(algo)));
+            all_algos.push(empties(algos.to_algo()));
         }
         if config.boost_few_empties_12 {
-            all_algos.push(algo_box(empties(booster(algo, 1, 2, 2.0))));
-            // all_algos.push(algo_box(empties(booster(algo, 1, 2, 3.0))));
-            // all_algos.push(algo_box(empties(booster(algo, 1, 2, 4.0))));
+            all_algos.push(empties(booster(algos.to_algo(), 1, 2, 2.0)));
+            // all_algos.push(empties(booster(algos.to_algo(), 1, 2, 3.0)));
+            // all_algos.push(empties(booster(algos.to_algo(), 1, 2, 4.0)));
         }
     }
 
     all_algos
 }
 
-fn algo_box<A: Algo + 'static>(algo: A) -> Box<dyn Algo> {
-    Box::new(algo) as Box<dyn Algo>
+fn scale(wrapped: Box<dyn Algo>, positive: bool) -> Box<dyn Algo> {
+    Box::new(MovesScaler { wrapped, positive })
 }
-fn scale<A: Algo>(wrapped: A, positive: bool) -> MovesScaler<A> {
-    MovesScaler { wrapped, positive }
-}
-fn booster<A: Algo>(
-    wrapped: A,
+fn booster(
+    wrapped: Box<dyn Algo>,
     min_value_to_boost: Card,
     max_value_to_boost: Card,
     boost: f64,
-) -> ValueBoosterWrapper<A> {
-    ValueBoosterWrapper {
+) -> Box<dyn Algo> {
+    Box::new(ValueBoosterWrapper {
         wrapped,
         min_value_to_boost,
         max_value_to_boost,
         boost,
-    }
+    })
 }
-fn empties<A: Algo>(wrapped: A) -> FewEmptiesScaler<A> {
-    FewEmptiesScaler { wrapped }
+fn empties(wrapped: Box<dyn Algo>) -> Box<dyn Algo> {
+    Box::new(FewEmptiesScaler { wrapped })
 }

threes_solver/src/algo/impls/empties.rs
@@ -1,17 +1,16 @@
 use threes_simulator::game_state::GameState;
 
-use crate::algo::core::Algos;
+use crate::algo::core::{Algo, Algos};
 
-use super::super::core::{assert_value_booster_not_supported, ValueBooster};
+use super::super::core::ValueBooster;
 
-impl Algos {
-    // cells that are empty
-    pub(crate) fn empties(
-        &self,
-        game_state: &GameState,
-        booster: Option<&dyn ValueBooster>,
-    ) -> f64 {
-        assert_value_booster_not_supported(self, booster);
+// cells that are empty
+#[derive(Debug)]
+pub(crate) struct Empties;
+
+impl Algo for Empties {
+    fn score(&self, game_state: &GameState, booster: Option<&dyn ValueBooster>) -> f64 {
+        self.assert_value_booster_not_supported(booster);
 
         game_state
             .get_grid()
@@ -19,14 +18,17 @@ impl Algos {
             .map(|&card| if card > 0 { 0.0 } else { 1.0 })
             .sum::<f64>()
     }
+
+    fn normalization_factor(&self) -> f64 {
+        Algos::ALGO_MAX_BASE / 16.0
+    }
 }
 
 /************ tests *************/
 
 #[cfg(test)]
 mod tests {
-    use crate::algo::core::Algos::Empties;
-    use crate::Algo;
+    use super::*;
 
     use super::super::super::test_utils::generate_game_state;
 
@@ -52,7 +54,7 @@ mod tests {
             0, 0, 0, 0,
         ]);
 
-        assert_eq!(8.0, Empties.empties(&game_state, None), "empty cells are counted correctly");
+        assert_eq!(8.0, Empties.score(&game_state, None), "empty cells are counted correctly");
     }
 
     #[test]
@@ -64,6 +66,6 @@ mod tests {
             0, 0, 0, 0,
             0, 0, 0, 0,
         ]);
-        assert_eq!(16.0, Empties.empties(&game_state, None), "empties max score");
+        assert_eq!(16.0, Empties.score(&game_state, None), "empties max score");
     }
 }

commit 4bd1318f381f44fe676f645fbd6f8cfcf7a83403

Fix bug where squeezes was positive, not negative
Introduced in e2e5221b9b1ff76f1abfbfabfb6c34d44e40f4f5

threes_solver/src/algo/algo_config.rs
@@ -70,15 +70,15 @@ fn default_config(algos: Algos) -> AlgoConfig {
             boost_few_empties_12: false,
         },
         Algos::Squeezes => AlgoConfig {
-            base: false,
+            base: true,
             time_positive: false,
             time_negative: false,
 
             boost_12: false,
             time_positive_boost_12: false,
-            time_negative_boost_12: false,
+            time_negative_boost_12: true,
 
-            boost_high: false,
+            boost_high: true,
 
             boost_few_empties: false,
             boost_few_empties_12: false,

threes_solver/src/algo/impls/squeezes.rs
@@ -34,7 +34,7 @@ impl Algo for Squeezes {
             }
         });
 
-        score
+        score * -1.0
     }
 
     fn normalization_factor(&self) -> f64 {
@@ -122,7 +122,7 @@ mod tests {
             0, 0, 0, 0,
         ]);
         assert_eq!(
-            2.0,
+            -2.0,
             Squeezes.score(&game_state, None),
             "1s and 2s can be squeezed by >= 3"
         );
@@ -146,7 +146,7 @@ mod tests {
             0, 6, 3, 6,
         ]);
         assert_eq!(
-            4.0,
+            -4.0,
             Squeezes.score(&game_state, None),
             "obvious squeezes in both directions"
         );
@@ -158,7 +158,7 @@ mod tests {
             3, 0, 6, 3,
         ]);
         assert_eq!(
-            4.0,
+            -4.0,
             Squeezes.score(&game_state, None),
             "you can be squeezed between a card and the wall, in all four directions"
         );
@@ -170,7 +170,7 @@ mod tests {
              0,  0, 0, 0,
         ]);
         assert_eq!(
-            1.0,
+            -1.0,
             Squeezes.score(&game_state, None),
             "the three is squeezed but I'm not sure it should be"
             // TODO handle this case better?
@@ -183,7 +183,7 @@ mod tests {
             0, 0, 0, 0,
         ]);
         assert_eq!(
-            1.0,
+            -1.0,
             Squeezes.score(&game_state, None),
             "the three counts as squeezed but that seems wrong"
             // TODO handle this case better?
@@ -197,7 +197,7 @@ mod tests {
          //  0  0   1   2
         ]);
         assert_eq!(
-            (2 + 2 + 1 + 0 + 0 + 0 + 1 + 2) as f64,
+            -(2 + 2 + 1 + 0 + 0 + 0 + 1 + 2) as f64,
             Squeezes.score(&game_state, None),
             "a big complex example"
         );
@@ -220,8 +220,8 @@ mod tests {
          //              ^- no squeezes (0)
         ]);
         assert_eq!(
-            1.0 * test_boost + 1.0 * test_boost + 2.0 * test_boost + 1.0
-                + 1.0 * test_boost + 0.0 + 1.0 + 1.0 * test_boost + 0.0,
+            -(1.0 * test_boost + 1.0 * test_boost + 2.0 * test_boost + 1.0
+                + 1.0 * test_boost + 0.0 + 1.0 + 1.0 * test_boost + 0.0),
             Squeezes.score(&game_state, Some(&booster)),
             "a big complex boosted example, covering a bunch of cases"
         );
@@ -236,7 +236,7 @@ mod tests {
             6, 6, 6, 6,
             3, 3, 3, 3,
         ]);
-        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, first case");
+        assert_eq!(-8.0, Squeezes.score(&game_state, None), "squeezes max score, first case");
 
         // 16
         let game_state = generate_game_state([
@@ -245,11 +245,11 @@ mod tests {
             3, 6, 3, 6,
             6, 3, 6, 3,
         ]);
-        assert_eq!(8.0 * 2.0, Squeezes.score(&game_state, None), "squeezes max score, second case");
+        assert_eq!(-8.0 * 2.0, Squeezes.score(&game_state, None), "squeezes max score, second case");
         assert_eq!(
-            super::super::ALGO_MAX_BASE,
+            -1.0 * super::super::ALGO_MAX_BASE,
             Squeezes.score(&game_state, None) * Squeezes.normalization_factor(),
-            "normalization of the highest score gives the overall max score"
+            "normalization of the highest score gives the overall max score (negative, in this case)"
         );
 
         let game_state = generate_game_state([
@@ -258,7 +258,7 @@ mod tests {
             3, 6, 3, 6,
             3, 6, 3, 6,
         ]);
-        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, third case");
+        assert_eq!(-8.0, Squeezes.score(&game_state, None), "squeezes max score, third case");
 
         let game_state = generate_game_state([
             3, 3, 3, 3,
@@ -266,6 +266,6 @@ mod tests {
             3, 6, 6, 3,
             3, 3, 3, 3,
         ]);
-        assert_eq!(8.0, Squeezes.score(&game_state, None), "squeezes max score, fourth case");
+        assert_eq!(-8.0, Squeezes.score(&game_state, None), "squeezes max score, fourth case");
     }
 }

threes_solver/weights.toml
@@ -1,10 +1,9 @@
 weights = [
-    2.493351086277598,
-    2.1202039884299206,
-    1.1688873596995288,
-    0.5407901798736829,
-    -0.4661104067301866,
-    1.4680808548395505,
-    0.36716821032573155,
-    0.2811810451497172,
+    4.105894442133954,
+    2.4360126896342553,
+    2.117804487467092,
+    0.5774793258225713,
+    1.9592871183147764,
+    0.45143528441002023,
+    0.2556825498005679,
 ]