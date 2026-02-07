d38512b568ee7154869aff7a8b7aef701e4e779e
Nathan Arthur <narthur-git@rainskit.com>
2026-02-07 15:13:31 -0500

    Hand-reviewed a good seed and made notes for improvements to --insights

--- threes_solver/simulate.sh
+++ threes_solver/simulate.sh
@@ -2,5 +2,15 @@
 
 set -eu
 
-cargo run --release -- --seed 291715960768821435 simulate "$@"
+cargo run --release -- simulate "$@"
+
+# 6144 with lookahead-depth 3
+# cargo run --release -- --lookahead-depth 3 --seed 6273387017077892403 simulate "$@"
+# but only 768 with lookahead-depth 2!
+# cargo run --release -- --seed 6273387017077892403 simulate "$@"
+
+# 3072
+# cargo run --release -- --seed 291715960768821435 simulate "$@"
+# cargo run --release -- --seed 16935663829556139934 simulate "$@"
+
 
--- threes_solver/simulate_to_success.sh
+++ threes_solver/simulate_to_success.sh
@@ -6,8 +6,9 @@ set -o pipefail
 OUTFILE=run_logs/sts.log
 
 while true; do
-	./simulate.sh > "$OUTFILE" 2>/dev/null
-	if grep -q 3072 "$OUTFILE" ; then
+	cargo run --release -- --lookahead-depth 3 simulate > "$OUTFILE" 2>/dev/null
+	# ./simulate.sh > "$OUTFILE" 2>/dev/null
+	if grep -q "11m6144" "$OUTFILE" ; then
 		grep seed "$OUTFILE"
 		break
 	fi
